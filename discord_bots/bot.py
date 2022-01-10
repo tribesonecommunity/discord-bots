7c7994f701941145f8a261e91288b06c0e93d511
dwayneyuen <dwayneyuen@gmail.com>
2022-01-09 21:24:07 -0800

    feat: make commands case insensitive

--- discord_bots/bot.py
+++ discord_bots/bot.py
@@ -12,4 +12,6 @@ intents.members = True
 load_dotenv()
 COMMAND_PREFIX: str = os.getenv("COMMAND_PREFIX") or "!"
 
-bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
+bot = commands.Bot(
+    case_insensitive=True, command_prefix=COMMAND_PREFIX, intents=intents
+)
