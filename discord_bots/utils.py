d6fba0e9cb5ecefaa917e413d80e082f87ad3734
root <dwayneyuen@gmail.com>
2022-02-14 20:53:48 -0800

    feat: add lt and stats

--- discord_bots/utils.py
+++ discord_bots/utils.py
@@ -1,12 +1,20 @@
 # Misc helper functions
+from datetime import datetime, timezone, tzinfo
 import itertools
 import math
+import os
 import statistics
-from datetime import datetime, timezone, tzinfo
 
+import discord
+from PIL import Image
+from discord.ext.commands.context import Context
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from trueskill import Rating, global_env
 
 from discord_bots.models import CurrentMap, Player, RotationMap, Session
+
+STATS_DIR = os.getenv("STATS_DIR")
 
 # Convenience mean function that can handle lists of 0 or 1 length
 def mean(values: list[any]) -> float:
@@ -45,6 +53,34 @@ def update_current_map_to_next_map_in_rotation():
             next_map = rotation_maps[0]
             session.add(CurrentMap(0, next_map.full_name, next_map.short_name))
         session.commit()
+
+
+async def upload_stats_screenshot(ctx: Context, cleanup=True):
+    # Assume the most recently modified HTML file is the correct stat sheet
+    html_files = list(filter(lambda x: x.endswith(".html"), os.listdir(STATS_DIR)))
+    html_files.sort(key=lambda x: os.path.getmtime(os.path.join(STATS_DIR, x)), reverse=True)
+
+    opts = FirefoxOptions()
+    opts.add_argument("--headless")
+    driver = webdriver.Firefox(options=opts)
+    if len(html_files) == 0:
+        return
+
+    driver.get("file://" + os.path.join(STATS_DIR, html_files[0]))
+    image_path = os.path.join(STATS_DIR, html_files[0] + ".png")
+    driver.save_screenshot(image_path)
+    image = Image.open(image_path)
+    # TODO: Un-hardcode these
+    cropped = image.crop((0, 0, 750, 650))
+    cropped.save(image_path)
+
+    await ctx.message.channel.send(file=discord.File(image_path))
+
+    # Clean up everything
+    if cleanup:
+        for file_ in os.listdir(STATS_DIR):
+            if file_.endswith(".png") or file_.endswith(".html"):
+                os.remove(os.path.join(STATS_DIR, file_))
 
 
 def win_probability(team0: list[Rating], team1: list[Rating]) -> float:
