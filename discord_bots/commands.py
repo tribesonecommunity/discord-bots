6ccf55999b4bce0297bf3e042e0528c184214772
Dwayne Yuen <dwayneyuen@gmail.com>
2023-11-23 10:55:40 -0800

    fix: show mu

--- discord_bots/commands.py
+++ discord_bots/commands.py
@@ -864,12 +864,14 @@ def finished_game_str(finished_game: FinishedGame, debug: bool = False) -> str:
             ),
             2,
         )
-    if debug:
-        team0_str = f"{finished_game.team0_name} ({team0_win_prob}%, mu: {team0_mu}, sigma: {team0_sigma}): {team0_names}"
-        team1_str = f"{finished_game.team1_name} ({team1_win_prob}%, mu: {team1_mu}, sigma: {team1_sigma}): {team1_names}"
-    else:
-        team0_str = f"{finished_game.team0_name} ({team0_win_prob}%): {team0_names}"
-        team1_str = f"{finished_game.team1_name} ({team1_win_prob}%): {team1_names}"
+    team0_str = f"{finished_game.team0_name} ({team0_win_prob}%, mu: {team0_mu}, sigma: {team0_sigma}): {team0_names}"
+    team1_str = f"{finished_game.team1_name} ({team1_win_prob}%, mu: {team1_mu}, sigma: {team1_sigma}): {team1_names}"
+    # if debug:
+    #     team0_str = f"{finished_game.team0_name} ({team0_win_prob}%, mu: {team0_mu}, sigma: {team0_sigma}): {team0_names}"
+    #     team1_str = f"{finished_game.team1_name} ({team1_win_prob}%, mu: {team1_mu}, sigma: {team1_sigma}): {team1_names}"
+    # else:
+    #     team0_str = f"{finished_game.team0_name} ({team0_win_prob}%): {team0_names}"
+    #     team1_str = f"{finished_game.team1_name} ({team1_win_prob}%): {team1_names}"
 
     if finished_game.winning_team == 0:
         output += f"\n**{team0_str}**"
