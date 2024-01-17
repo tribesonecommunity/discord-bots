1eee83261c83f019379fd62415a64b742cba2a35
Dwayne Yuen <dwayneyuen@gmail.com>
2024-01-16 22:23:25 -0800

    refactor: change debug command to ENABLE_DEBUG

--- discord_bots/config.py
+++ discord_bots/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Convenience function to print the id of a channel
-SHOW_CHANNEL_ID = bool(os.getenv("SHOW_CHANNEL_ID"))
+ENABLE_DEBUG = bool(os.getenv("ENABLE_DEBUG"))
 SHOW_TRUESKILL = bool(os.getenv("SHOW_TRUESKILL"))
 DISABLE_MAP_ROTATION = bool(os.getenv("DISABLE_MAP_ROTATION"))
 LEADERBOARD_CHANNEL = int(os.getenv("LEADERBOARD_CHANNEL") or "0")
