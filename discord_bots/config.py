b31182d04891e8049d1c5bfa6125337e3888e91f
Dwayne Yuen <dwayneyuen@gmail.com>
2024-01-16 22:00:37 -0800

    feat: helper show channel id function

--- discord_bots/config.py
+++ discord_bots/config.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Convenience function to print the id of a channel
+SHOW_CHANNEL_ID = bool(os.getenv("SHOW_CHANNEL_ID"))
 SHOW_TRUESKILL = bool(os.getenv("SHOW_TRUESKILL"))
 DISABLE_MAP_ROTATION = bool(os.getenv("DISABLE_MAP_ROTATION"))
 LEADERBOARD_CHANNEL = int(os.getenv("LEADERBOARD_CHANNEL") or "0")
