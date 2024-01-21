3737e99a181710de7ce8cca41f18bfcb174c0c4b
Drew Layton <dlayton66@gmail.com>
2024-01-21 15:48:28 -0600

    refactor: add BaseCog to RotationCog

--- discord_bots/cogs/rotation.py
+++ discord_bots/cogs/rotation.py
@@ -1,73 +1,55 @@
-from discord import Colour
-from discord.ext.commands import Bot, Cog, Context, check, command
+from discord.ext.commands import Bot, Context, check, command
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
 
 from discord_bots.checks import is_admin
+from discord_bots.cogs.base import BaseCog
 from discord_bots.models import Map, Queue, Rotation, RotationMap
-from discord_bots.utils import send_message
 
 
-class RotationCog(Cog):
+class RotationCog(BaseCog):
     def __init__(self, bot: Bot):
-        self.bot = bot
+        super().__init__(bot)
 
     @command()
     @check(is_admin)
     async def addrotation(self, ctx: Context, rotation_name: str):
-        message = ctx.message
         session = ctx.session
-        session.add(Rotation(name=rotation_name))
 
         try:
+            session.add(Rotation(name=rotation_name))
             session.commit()
         except IntegrityError:
             session.rollback()
-            await send_message(
-                message.channel,
-                embed_description=f"Error adding rotation {rotation_name}). Does it already exist?",
-                colour=Colour.red(),
+            await self.send_error_message(
+                f"Error adding rotation {rotation_name}). Does it already exist?"
             )
         else:
-            await send_message(
-                message.channel,
-                embed_description=f"{rotation_name} added to rotation pool",
-                colour=Colour.green(),
-            )
+            await self.send_success_message(f"{rotation_name} added to rotation pool")
 
     @command(usage="<rotation_name> <map_short_name> <position>")
     @check(is_admin)
     async def addrotationmap(
         self, ctx: Context, rotation_name: str, map_short_name: str, ordinal: int
     ):
-        message = ctx.message
         session = ctx.session
-        rotation: Rotation | None = (
-            session.query(Rotation).filter(Rotation.name.ilike(rotation_name)).first()
-        )
-        map: Map | None = (
-            session.query(Map).filter(Map.short_name.ilike(map_short_name)).first()
-        )
 
         if ordinal < 1:
-            await send_message(
-                message.channel,
-                embed_description="Position must be a positive number",
-                colour=Colour.red(),
-            )
+            await self.send_error_message("Position must be a positive number")
             return
-        if not rotation:
-            await send_message(
-                message.channel,
-                embed_description=f"Could not find rotation: {rotation_name}",
-                colour=Colour.red(),
+
+        try:
+            rotation = (
+                session.query(Rotation).filter(Rotation.name.ilike(rotation_name)).one()
             )
+        except NoResultFound:
+            await self.send_error_message(f"Could not find rotation: {rotation_name}")
             return
-        if not map:
-            await send_message(
-                message.channel,
-                embed_description=f"Could not find map: {map_short_name}",
-                colour=Colour.red(),
-            )
+
+        try:
+            map = session.query(Map).filter(Map.short_name.ilike(map_short_name)).one()
+        except NoResultFound:
+            await self.send_error_message(f"Could not find map: {map_short_name}")
             return
 
         # logic for organizing ordinals.  ordinals are kept unique and consecutive.
@@ -85,86 +67,69 @@ class RotationCog(Cog):
             for rotation_map in rotation_maps[ordinal - 1 :]:
                 rotation_map.ordinal += 1
 
-        session.add(
-            RotationMap(rotation_id=rotation.id, map_id=map.id, ordinal=ordinal)
-        )
-
         try:
+            session.add(
+                RotationMap(rotation_id=rotation.id, map_id=map.id, ordinal=ordinal)
+            )
             session.commit()
         except IntegrityError:
             session.rollback()
-            await send_message(
-                message.channel,
-                embed_description=f"Error adding {map_short_name} to {rotation_name} at position {ordinal}",
-                colour=Colour.red(),
+            await self.send_error_message(
+                f"Error adding {map_short_name} to {rotation_name} at position {ordinal}"
             )
         else:
-            await send_message(
-                message.channel,
-                embed_description=f"{map.short_name} added to {rotation.name} at position {ordinal}",
-                colour=Colour.green(),
+            await self.send_success_message(
+                f"{map.short_name} added to {rotation.name} at position {ordinal}"
             )
 
     @command()
     async def listrotations(self, ctx: Context):
-        message = ctx.message
         session = ctx.session
+
         data = (
             session.query(Rotation.name, RotationMap.ordinal, Map.short_name)
-            .join(RotationMap, Rotation.id == RotationMap.rotation_id)
-            .join(Map, Map.id == RotationMap.map_id)
+            .outerjoin(RotationMap, Rotation.id == RotationMap.rotation_id)
+            .outerjoin(Map, Map.id == RotationMap.map_id)
             .order_by(RotationMap.ordinal.asc())
             .all()
         )
-        grouped_data = {}
 
+        grouped_data = {}
         for row in data:
             if row[0] in grouped_data:
                 grouped_data[row[0]].append(row[2])
+            elif not row[2]:
+                grouped_data[row[0]] = ["--No Maps--"]
             else:
                 grouped_data[row[0]] = [row[2]]
 
         output = ""
-
         for key, value in grouped_data.items():
             output += f"**- {key}**"
             output += f"_{', '.join(value)}_\n\n"
 
-        await send_message(
-            message.channel, embed_description=output, colour=Colour.blue()
-        )
+        await self.send_info_message(output)
 
     @command()
     async def setqueuerotation(self, ctx: Context, queue_name: str, rotation_name: str):
-        message = ctx.message
         session = ctx.session
 
-        queue = session.query(Queue).filter(Queue.name.ilike(queue_name)).first()
-        rotation = (
-            session.query(Rotation).filter(Rotation.name.ilike(rotation_name)).first()
-        )
-
-        if not queue:
-            await send_message(
-                message.channel,
-                embed_description=f"Could not find queue {queue_name}",
-                colour=Colour.red(),
-            )
+        try:
+            queue = session.query(Queue).filter(Queue.name.ilike(queue_name)).one()
+        except NoResultFound:
+            await self.send_error_message(f"Could not find queue: {queue_name}")
             return
-        if not rotation:
-            await send_message(
-                message.channel,
-                embed_description=f"Could not find rotation {rotation_name}",
-                colour=Colour.red(),
+
+        try:
+            rotation = (
+                session.query(Rotation).filter(Rotation.name.ilike(rotation_name)).one()
             )
+        except NoResultFound:
+            await self.send_error_message(f"Could not find rotation: {rotation_name}")
             return
 
         queue.rotation_id = rotation.id
-
-        await send_message(
-            message.channel,
-            embed_description=f"Rotation for {queue.name} set to {rotation.name}",
-            colour=Colour.green(),
-        )
-
         session.commit()
+        await self.send_success_message(
+            f"Rotation for {queue.name} set to {rotation.name}"
+        )
