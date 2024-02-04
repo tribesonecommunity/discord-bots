ea97eb6a96360028d6548eaab8eee7f016bac362
Drew Layton <dlayton66@gmail.com>
2024-02-04 13:14:54 -0600

    feat: setname method in basecog

--- discord_bots/cogs/base.py
+++ discord_bots/cogs/base.py
@@ -29,3 +29,22 @@ class BaseCog(Cog):
         await send_message(
             self.message.channel, embed_description=error_message, colour=Colour.red()
         )
+
+    async def setname(self, ctx: Context, class_, old_name: str, new_name: str):
+        session = ctx.session
+
+        entry: class_ | None = (
+            session.query(class_).filter(class_.name.ilike(old_name)).first()
+        )
+        if not entry:
+            await self.send_error_message(
+                f"Could not find {class_.__name__.lower()} **{old_name}**"
+            )
+            return
+
+        old_name = entry.name
+        entry.name = new_name
+        session.commit()
+        await self.send_success_message(
+            f"{class_.__name__} name updated from **{old_name}** to **{new_name}**"
+        )
