3818ec3fd08b2e4c1cf7e96906e17774091e241c
Andrewki44 <andrewkilbourn1996@gmail.com>
2024-02-07 21:20:42 +1100

    Add move_enabled flag to player and queue tables

---
+++ alembic/versions/20240207210853_b29ddf680a84_add_move_option_to_player_and_queue.py
@@ -0,0 +1,46 @@
+"""add move option to player and queue
+
+Revision ID: b29ddf680a84
+Revises: 537a7815efa5
+Create Date: 2024-02-07 21:08:53.662450
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "b29ddf680a84"
+down_revision = "537a7815efa5"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table("player", schema=None) as batch_op:
+        batch_op.add_column(
+            sa.Column(
+                "move_enabled",
+                sa.Boolean(),
+                server_default=sa.text("0"),
+                nullable=False,
+            )
+        )
+    
+    with op.batch_alter_table("queue", schema=None) as batch_op:
+        batch_op.add_column(
+            sa.Column(
+                "move_enabled",
+                sa.Boolean(),
+                server_default=sa.text("0"),
+                nullable=False,
+            )
+        )
+
+
+def downgrade():
+    with op.batch_alter_table("player", schema=None) as batch_op:
+        batch_op.drop_column("move_enabled")
+
+    with op.batch_alter_table("queue", schema=None) as batch_op:
+        batch_op.drop_column("move_enabled")
