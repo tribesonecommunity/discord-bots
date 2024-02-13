48abbdda1ee316a26150b283578d37420c59d1fa
Andrewki44 <andrewkilbourn1996@gmail.com>
2024-02-14 08:19:22 +1100

    Revert Create_Game to prepare for sync from upstream

--- discord_bots/commands.py
+++ discord_bots/commands.py
@@ -377,6 +377,13 @@ async def create_game(
         be_channel, ds_channel = await create_team_voice_channels(
             session, guild, game, voice_category
         )
+        if config.ENABLE_VOICE_MOVE and queue.move_enabled:
+            await _movegameplayers(short_game_id, None, guild)
+            await send_message(
+                channel,
+                embed_description=f"Players moved to voice channels for game {short_game_id}",
+                colour=Colour.green(),
+            )
     else:
         print(
             f"could not find tribes_voice_category with id {config.TRIBES_VOICE_CATEGORY_CHANNEL_ID} in guild"
@@ -412,14 +419,6 @@ async def create_game(
 
     if not rolled_random_map:
         await update_next_map_to_map_after_next(queue.rotation_id, False)
-
-    if config.ENABLE_VOICE_MOVE and queue.move_enabled:
-        await _movegameplayers(short_game_id, None, guild)
-        await send_message(
-            channel,
-            embed_description=f"Players moved to voice channels for game {short_game_id}",
-            colour=Colour.green(),
-                )
 
     session.close()
 
