2c632c1192c3ee821896de9cb90f32240573b287
StephenBartos <stephencbartos@gmail.com>
2024-03-18 17:50:38 -0400

    fix: use context manager in on_message

--- discord_bots/main.py
+++ discord_bots/main.py
@@ -2,12 +2,11 @@ import asyncio
 import logging
 from datetime import datetime, timezone
 
-import discord.utils
+import sqlalchemy
 from discord import Colour, Embed, Interaction, Member, Message, Reaction
 from discord.abc import User
 from discord.app_commands import AppCommandError, errors
 from discord.ext.commands import CommandError, Context, UserInputError
-from sqlalchemy.orm.session import Session as SQLAlchemySession
 
 import discord_bots.config as config
 from discord_bots.cogs.categories import CategoryCommands
@@ -20,7 +19,14 @@ from discord_bots.cogs.rotation import RotationCommands
 from discord_bots.cogs.vote import VoteCommands
 
 from .bot import bot
-from .models import CustomCommand, Player, QueuePlayer, QueueWaitlistPlayer, Session
+from .models import (
+    CustomCommand,
+    Player,
+    QueuePlayer,
+    QueueWaitlistPlayer,
+    Session,
+    engine,
+)
 from .tasks import (
     add_player_task,
     afk_timer_task,
@@ -137,25 +143,25 @@ async def on_message(message: Message):
     if (config.CHANNEL_ID and message.channel.id == config.CHANNEL_ID) or (
         config.LEADERBOARD_CHANNEL and message.channel.id == config.LEADERBOARD_CHANNEL
     ):
-        session: SQLAlchemySession = Session()
-        player: Player | None = (
-            session.query(Player).filter(Player.id == message.author.id).first()
-        )
-        if player:
-            player.last_activity_at = datetime.now(timezone.utc)
-            if player.name != message.author.display_name:
-                player.name = message.author.display_name
-        else:
-            session.add(
-                Player(
-                    id=message.author.id,
-                    name=message.author.display_name,
-                    last_activity_at=datetime.now(timezone.utc),
-                    currency=config.STARTING_CURRENCY,
-                )
+        session: sqlalchemy.orm.Session
+        with Session() as session:
+            player: Player | None = (
+                session.query(Player).filter(Player.id == message.author.id).first()
             )
-        session.commit()
-        session.close()
+            if player:
+                player.last_activity_at = datetime.now(timezone.utc)
+                if player.name != message.author.display_name:
+                    player.name = message.author.display_name
+            else:
+                session.add(
+                    Player(
+                        id=message.author.id,
+                        name=message.author.display_name,
+                        last_activity_at=datetime.now(timezone.utc),
+                        currency=config.STARTING_CURRENCY,
+                    )
+                )
+            session.commit()
         await bot.process_commands(message)
 
         # Custom commands below
@@ -164,16 +170,15 @@ async def on_message(message: Message):
 
         bot_commands = {command.name for command in bot.commands}
         command_name = message.content.split(" ")[0][1:]
-        session = Session()
-        if command_name not in bot_commands:
-            custom_command: CustomCommand | None = (
-                session.query(CustomCommand)
-                .filter(CustomCommand.name == command_name)
-                .first()
-            )
-            if custom_command:
-                await message.channel.send(content=custom_command.output)
-        session.close()
+        with Session() as session:
+            if command_name not in bot_commands:
+                custom_command: CustomCommand | None = (
+                    session.query(CustomCommand)
+                    .filter(CustomCommand.name == command_name)
+                    .first()
+                )
+                if custom_command:
+                    await message.channel.send(content=custom_command.output)
 
 
 @bot.event
