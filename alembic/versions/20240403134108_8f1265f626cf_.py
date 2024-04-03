a00fc8196cb32831d602e294f34aa25f46d59857
Drew Layton <dlayton66@gmail.com>
2024-04-03 13:42:43 -0500

    feat: merge alembic histories

---
+++ alembic/versions/20240403134108_8f1265f626cf_.py
@@ -0,0 +1,25 @@
+"""Merge 1a712c98b3d9 and d4bd406cc25c
+
+Revision ID: 8f1265f626cf
+Revises: 1a712c98b3d9, d4bd406cc25c
+Create Date: 2024-04-03 13:41:08.333291
+
+"""
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "8f1265f626cf"
+down_revision = ("1a712c98b3d9", "d4bd406cc25c")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
