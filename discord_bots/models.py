57301d78f3f40d573da5dc8430d9ac31d38f3205
Drew Layton <dlayton66@gmail.com>
2024-04-06 18:02:24 -0500

    fix: remove utc default factory - time objects don't store timezone

--- discord_bots/models.py
+++ discord_bots/models.py
@@ -1297,10 +1297,7 @@ class Schedule:
     __table_args__ = (UniqueConstraint("day", "time"),)
 
     day: str = field(metadata={"sa": Column(String, nullable=False)})
-    time: time = field(
-        metadata={"sa": Column(Time, nullable=False)},
-        default_factory=lambda: datetime.now(timezone.utc),
-    )
+    time: time = field(metadata={"sa": Column(Time, nullable=False)})
     message_id: int | None = field(
         default=None, metadata={"sa": Column(BigInteger, nullable=True)}
     )
